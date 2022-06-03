6149a819e53d31f49695937fb4879067b6b6992c
Ian Hill <ianhill@Ians-MBP.home>
2022-06-02 20:40:09 -0400

    added five day forecast

--- javascript.js
+++ javascript.js
@@ -3,8 +3,32 @@
 // forecastEl
 var searchTextEl = document.getElementById("search-text");
 var searchButtonEl = document.getElementById("search-button");
+var forecastEl = document.getElementById("forecast");
 
 
+function buildForecast(data){
+  for(i=0; i<5; i++){
+    var parentCard = document.createElement("div").classList.add("card");
+    var cardHeader = document.createElement("div").classList.add("card-header");
+    var weatherDataList = document.createElement("ul").classList.add("list-group list-group-flush");
+    var icon = document.createElement("img");
+    var iconHolder = document.createElement("li").classList.add("list-group-item");
+    var currentTemp = document.createElement("li").classList.add("list-group-item");
+    var currentConditions = document.createElement("li").classList.add("list-group-item");
+
+    currentTemp.textContent = data.daily[i].temp.day;
+    currentConditions.textContent = data.daily[i].weather[0].main;
+    icon.src = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`
+    cardHeader.textContent = "today's date"
+    iconHolder.appendChild(icon);
+    weatherDataList.appendChild(currentTemp);
+    weatherDataList.appendChild(currentConditions);
+    weatherDataList.appendChild(iconHolder);
+    parentCard.appendChild(cardHeader);
+    parentCard.appendChild(weatherDataList);
+    forecastEl.appendChild(parentCard);
+  }
+}
 
 
 function getWeather(city) {
@@ -25,8 +49,7 @@ function getWeather(city) {
     })
     .then(function (data){
         console.log(data);
-
-
+        buildForecast(data);
     })
   });
 } 
